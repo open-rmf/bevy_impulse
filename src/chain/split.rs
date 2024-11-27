68b48bce41e869f696bac242b194deaae8b793b0
Michael X. Grey <greyxmike@gmail.com>
2024-11-27 18:46:36 +0800

    Test array key limit
    Signed-off-by: Michael X. Grey <greyxmike@gmail.com>

--- src/chain/split.rs
+++ src/chain/split.rs
@@ -941,4 +941,49 @@ mod tests {
         assert_eq!(result["fib"], input_map["fib"]);
         assert_eq!(result["dib"], input_map["dib"]);
     }
+
+    #[test]
+    fn test_array_split_limit() {
+        let mut context = TestingContext::minimal_plugins();
+
+        let workflow = context.spawn_io_workflow(|scope, builder| {
+            scope.input.chain(builder).split(|split| {
+                let err = split
+                    .next_branch(|_, chain| {
+                        chain.value().connect(scope.terminate);
+                    })
+                    .unwrap()
+                    .next_branch(|_, chain| {
+                        chain.value().connect(scope.terminate);
+                    })
+                    .unwrap()
+                    .next_branch(|_, chain| {
+                        chain.value().connect(scope.terminate);
+                    })
+                    .unwrap()
+                    .next_branch(|_, chain| {
+                        chain.value().connect(scope.terminate);
+                    })
+                    .unwrap()
+                    // This last one should fail because it should exceed the
+                    // array limit
+                    .next_branch(|_, chain| {
+                        chain.value().connect(scope.terminate);
+                    });
+
+                assert!(matches!(err, Err(_)));
+            })
+        });
+
+        let mut promise =
+            context.command(|commands| commands.request([1, 2, 3, 4], workflow).take_response());
+
+        context.run_with_conditions(&mut promise, 1);
+        assert!(context.no_unhandled_errors());
+
+        let result = promise.take().available().unwrap();
+        // All the values in the array are racing to finish, but the first value
+        // should finish first since it will naturally get queued first.
+        assert_eq!(result, 1);
+    }
 }
