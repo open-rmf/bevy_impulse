3cbbd137c922cbe4616ab108a3f481723ffdd4cf
Teo Koon Peng <teokoonpeng@gmail.com>
2025-01-03 03:39:37 +0000

    use clap; use ImpulsePlugin; fix multiply3.json
    Signed-off-by: Teo Koon Peng <teokoonpeng@gmail.com>

--- examples/diagram/calculator/src/main.rs
+++ examples/diagram/calculator/src/main.rs
@@ -1,9 +1,21 @@
 use std::{error::Error, fs::File, str::FromStr};
 
-use bevy_app::Update;
-use bevy_impulse::{flush_impulses, Diagram, NodeRegistry, RequestExt, RunCommandsOnWorldExt};
+use bevy_impulse::{Diagram, ImpulsePlugin, NodeRegistry, RequestExt, RunCommandsOnWorldExt};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+/// Example calculator app using diagrams.
+struct Args {
+    #[arg(help = "path to the diagram to run")]
+    diagram: String,
+
+    #[arg(help = "json containing the request to the diagram")]
+    request: String,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
     tracing_subscriber::fmt::init();
 
     let mut registry = NodeRegistry::default();
@@ -20,17 +32,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         builder.create_map_block(move |req: f64| req / config)
     });
 
-    let mut args = std::env::args();
-    args.next();
-    let path = args.next().unwrap();
-    let request_str = args.next().unwrap();
-
     let mut app = bevy_app::App::new();
-    app.add_systems(Update, flush_impulses());
-    let file = File::open(path).unwrap();
+    app.add_plugins(ImpulsePlugin::default());
+    let file = File::open(args.diagram).unwrap();
     let diagram = Diagram::from_reader(file)?;
 
-    let request = serde_json::Value::from_str(&request_str)?;
+    let request = serde_json::Value::from_str(&args.request)?;
     let workflow = diagram.spawn_io_workflow(&mut app, &registry)?;
     let mut promise = app
         .world
