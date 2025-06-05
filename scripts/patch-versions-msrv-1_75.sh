a690628f64ce672f52e1a6b568970837ebb87a03
Michael X. Grey <greyxmike@gmail.com>
2025-06-05 19:04:40 +0800

    Remove Rust 1.75 from CI
    Signed-off-by: Michael X. Grey <greyxmike@gmail.com>

--- scripts/patch-versions-msrv-1_75.sh
+++
@@ -1,5 +0,0 @@
-# This script is useful for forcing dependencies to be compatible with Rust v1.75
-# Run this script in the root directory of the package.
-
-cargo add home@=0.5.9
-cargo add backtrace@=0.3.74
