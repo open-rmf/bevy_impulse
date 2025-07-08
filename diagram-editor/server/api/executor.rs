c7cc4bf2648d344b73ec6e3fe506d45530232f57
Teo Koon Peng <teokoonpeng@gmail.com>
2025-07-08 05:27:00 +0000

    add placeholder debug api
    Signed-off-by: Teo Koon Peng <teokoonpeng@gmail.com>

--- diagram-editor/server/api/executor.rs
+++ diagram-editor/server/api/executor.rs
@@ -1,17 +1,27 @@
 use std::{
+    fmt::Display,
     sync::{Arc, Mutex},
     time::Duration,
 };
 
-use axum::{extract::State, http::StatusCode, routing::post, Json, Router};
+use axum::{
+    extract::{
+        ws::{self, Utf8Bytes},
+        State,
+    },
+    http::StatusCode,
+    routing::{self, post},
+    Json, Router,
+};
 use bevy_ecs::{
     event::EventWriter,
     system::{ResMut, Resource},
 };
 use bevy_impulse::{Diagram, DiagramElementRegistry, Promise, RequestExt};
-use serde::Deserialize;
+use futures_util::{Sink, SinkExt, Stream, StreamExt};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use tokio::sync::mpsc::error::TryRecvError;
-use tracing::error;
+use tracing::{debug, error};
 
 struct Context {
     diagram: Diagram,
@@ -73,6 +83,100 @@ async fn post_run(
     Ok(Json(response))
 }
 
+async fn recv_text<R>(read: &mut R) -> Option<Utf8Bytes>
+where
+    R: Stream<Item = Result<ws::Message, axum::Error>> + Unpin,
+{
+    let msg = if let Some(msg) = read.next().await {
+        match msg {
+            Ok(msg) => msg,
+            Err(err) => {
+                debug!("{}", err);
+                return None;
+            }
+        }
+    } else {
+        return None;
+    };
+    let msg_text = match msg.into_text() {
+        Ok(text) => text,
+        Err(err) => {
+            debug!("{}", err);
+            return None;
+        }
+    };
+    Some(msg_text)
+}
+
+async fn recv_json<T: DeserializeOwned, R>(read: &mut R) -> Option<T>
+where
+    R: Stream<Item = Result<ws::Message, axum::Error>> + Unpin,
+{
+    let text = recv_text(read).await?;
+    match serde_json::from_slice(text.as_bytes()) {
+        Ok(value) => Some(value),
+        Err(err) => {
+            debug!("{}", err);
+            None
+        }
+    }
+}
+
+async fn send_json<T: Serialize, W>(write: &mut W, value: &T) -> Option<()>
+where
+    W: Sink<ws::Message> + Unpin,
+    W::Error: Display,
+{
+    let json_str = match serde_json::to_string(value).into() {
+        Ok(json_str) => json_str,
+        Err(err) => {
+            debug!("{}", err);
+            return None;
+        }
+    };
+    match write.send(ws::Message::Text(json_str.into())).await {
+        Ok(_) => Some(()),
+        Err(err) => {
+            debug!("{}", err);
+            None
+        }
+    }
+}
+
+#[cfg_attr(test, derive(serde::Deserialize))]
+#[derive(Serialize)]
+enum DebugSessionEnd {
+    Ok(serde_json::Value),
+    Err(String),
+}
+
+/// Start a debug session.
+async fn ws_debug<W, R>(mut write: W, mut read: R, state: State<ExecutorState>)
+where
+    W: Sink<ws::Message> + Unpin,
+    W::Error: Display,
+    R: Stream<Item = Result<ws::Message, axum::Error>> + Unpin,
+{
+    let req: PostRunRequest = if let Some(req) = recv_json(&mut read).await {
+        req
+    } else {
+        return;
+    };
+
+    // TODO: bevy_impulse does not support debugging yet, for now just run the diagram to terminate.
+    let response = post_run(state, Json(req)).await;
+    match response {
+        Ok(resp) => {
+            send_json(&mut write, &DebugSessionEnd::Ok(resp.0)).await;
+            return;
+        }
+        Err(status_code) => {
+            send_json(&mut write, &DebugSessionEnd::Err(status_code.to_string())).await;
+            return;
+        }
+    }
+}
+
 #[derive(Resource)]
 struct RequestReceiver(tokio::sync::mpsc::Receiver<Context>);
 
@@ -118,23 +222,42 @@ impl Default for ExecutorOptions {
     }
 }
 
+fn setup_bevy_app(
+    app: &mut bevy_app::App,
+    registry: DiagramElementRegistry,
+    options: &ExecutorOptions,
+) -> ExecutorState {
+    let (request_tx, request_rx) = tokio::sync::mpsc::channel::<Context>(10);
+    app.insert_resource(RequestReceiver(request_rx));
+    app.add_systems(bevy_app::Update, execute_requests);
+    ExecutorState {
+        registry: Arc::new(Mutex::new(registry)),
+        send_chan: request_tx,
+        response_timeout: options.response_timeout,
+    }
+}
+
 pub(super) fn new_router(
     app: &mut bevy_app::App,
     registry: DiagramElementRegistry,
     options: ExecutorOptions,
 ) -> Router {
-    let (tx, rx) = tokio::sync::mpsc::channel::<Context>(10);
-
-    app.insert_resource(RequestReceiver(rx));
-    app.add_systems(bevy_app::Update, execute_requests);
+    let executor_state = setup_bevy_app(app, registry, &options);
 
     Router::new()
         .route("/run", post(post_run))
-        .with_state(ExecutorState {
-            registry: Arc::new(Mutex::new(registry)),
-            send_chan: tx,
-            response_timeout: options.response_timeout,
-        })
+        .route(
+            "/debug",
+            routing::any(
+                async |ws: ws::WebSocketUpgrade, state: State<ExecutorState>| {
+                    ws.on_upgrade(|socket| {
+                        let (write, read) = socket.split();
+                        ws_debug(write, read, state)
+                    })
+                },
+            ),
+        )
+        .with_state(executor_state)
 }
 
 #[cfg(test)]
@@ -149,17 +272,47 @@ mod tests {
     use bevy_impulse::{ImpulseAppPlugin, NodeBuilderOptions};
     use mime_guess::mime;
     use serde_json::json;
-    use tower::Service;
+    use tower::ServiceExt;
 
-    #[tokio::test]
-    #[test_log::test]
-    async fn test_post_run() {
+    struct TestFixture<CleanupFn> {
+        router: Router,
+        cleanup_test: CleanupFn,
+    }
+
+    fn setup_test() -> TestFixture<impl FnOnce()> {
         let mut registry = DiagramElementRegistry::new();
         registry.register_node_builder(NodeBuilderOptions::new("add7"), |builder, _config: ()| {
             builder.create_map_block(|req: i32| req + 7)
         });
 
-        let diagram = Diagram::from_json(json!({
+        let mut app = bevy_app::App::new();
+        app.add_plugins(ImpulseAppPlugin::default());
+        let (send_stop, mut recv_stop) = tokio::sync::oneshot::channel::<()>();
+        app.add_systems(
+            bevy_app::Update,
+            move |mut app_exit: EventWriter<bevy_app::AppExit>| {
+                if let Ok(_) = recv_stop.try_recv() {
+                    app_exit.send_default();
+                }
+            },
+        );
+
+        let router = new_router(&mut app, registry, ExecutorOptions::default());
+        let join_handle = thread::spawn(move || {
+            app.run();
+        });
+
+        TestFixture {
+            router,
+            cleanup_test: move || {
+                send_stop.send(()).unwrap();
+                join_handle.join().unwrap();
+            },
+        }
+    }
+
+    fn new_add7_diagram() -> Diagram {
+        Diagram::from_json(json!({
             "version": "0.1.0",
             "start": "add7",
             "ops": {
@@ -170,31 +323,25 @@ mod tests {
                 },
             },
         }))
-        .unwrap();
+        .unwrap()
+    }
 
-        let mut app = bevy_app::App::new();
-        app.add_plugins(ImpulseAppPlugin::default());
-        let (send_stop, mut recv_stop) = tokio::sync::oneshot::channel();
-        app.add_systems(
-            bevy_app::Update,
-            move |mut app_exit: EventWriter<bevy_app::AppExit>| {
-                if let Ok(_) = recv_stop.try_recv() {
-                    app_exit.send_default();
-                }
-            },
-        );
+    #[tokio::test]
+    #[test_log::test]
+    async fn test_post_run() {
+        let TestFixture {
+            router,
+            cleanup_test,
+        } = setup_test();
 
-        let mut router = new_router(&mut app, registry, ExecutorOptions::default());
-        let join_handle = thread::spawn(move || {
-            app.run();
-        });
+        let diagram = new_add7_diagram();
 
         let request_body = PostRunRequest {
             diagram,
             request: serde_json::Value::from(5),
         };
         let response = router
-            .call(
+            .oneshot(
                 Request::post("/run")
                     .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.to_string())
                     .body(serde_json::to_string(&request_body).unwrap())
@@ -219,7 +366,86 @@ mod tests {
         let resp: i32 = serde_json::from_str(resp_str).unwrap();
         assert_eq!(resp, 12);
 
-        send_stop.send(()).unwrap();
-        join_handle.join().unwrap();
+        cleanup_test();
+    }
+
+    struct WsTestFixture<CleanupFn> {
+        executor_state: ExecutorState,
+        cleanup_test: CleanupFn,
+    }
+
+    fn setup_ws_test() -> WsTestFixture<impl FnOnce()> {
+        let mut app = bevy_app::App::new();
+        app.add_plugins(ImpulseAppPlugin::default());
+        let (send_stop, mut recv_stop) = tokio::sync::oneshot::channel::<()>();
+        app.add_systems(
+            bevy_app::Update,
+            move |mut app_exit: EventWriter<bevy_app::AppExit>| {
+                if let Ok(_) = recv_stop.try_recv() {
+                    app_exit.send_default();
+                }
+            },
+        );
+
+        let mut registry = DiagramElementRegistry::new();
+        registry.register_node_builder(NodeBuilderOptions::new("add7"), |builder, _config: ()| {
+            builder.create_map_block(|req: i32| req + 7)
+        });
+        let executor_state = setup_bevy_app(&mut app, registry, &ExecutorOptions::default());
+
+        let join_handle = thread::spawn(move || {
+            app.run();
+        });
+
+        WsTestFixture {
+            executor_state,
+            cleanup_test: move || {
+                send_stop.send(()).unwrap();
+                join_handle.join().unwrap();
+            },
+        }
+    }
+
+    #[tokio::test]
+    #[test_log::test]
+    async fn test_ws_debug() {
+        let WsTestFixture {
+            executor_state,
+            cleanup_test,
+        } = setup_ws_test();
+
+        let diagram = new_add7_diagram();
+
+        let request_body = PostRunRequest {
+            diagram,
+            request: serde_json::Value::from(5),
+        };
+
+        // Need to use "futures" channels rather than "tokio" channels as they implement `Sink` and
+        // `Stream`
+        let (socket_write, mut test_rx) = futures_channel::mpsc::channel(1024);
+        let (mut test_tx, socket_read) = futures_channel::mpsc::channel(1024);
+
+        tokio::spawn(ws_debug(socket_write, socket_read, State(executor_state)));
+
+        test_tx
+            .send(Ok(ws::Message::Text(
+                serde_json::to_string(&request_body).unwrap().into(),
+            )))
+            .await
+            .unwrap();
+
+        let resp_msg = test_rx.next().await.unwrap();
+        let resp_text = resp_msg.into_text().unwrap();
+        let resp: DebugSessionEnd = serde_json::from_slice(resp_text.as_bytes()).unwrap();
+        let resp = match resp {
+            DebugSessionEnd::Ok(resp) => resp,
+            _ => {
+                panic!("expected response to be Ok");
+            }
+        };
+        assert_eq!(resp, serde_json::Value::from(12));
+
+        cleanup_test();
     }
 }
