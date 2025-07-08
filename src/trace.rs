2becb7ae685a7eb64442e3b874690afacc7d0490
Michael X. Grey <greyxmike@gmail.com>
2025-07-08 23:34:39 +0800

    Drafting trace capabilities
    Signed-off-by: Michael X. Grey <greyxmike@gmail.com>

--- src/trace.rs
+++ src/trace.rs
@@ -15,19 +15,82 @@
  *
 */
 
-use std::sync::Arc;
+use crate::{OperationName, OperationRef, BuilderId, JsonMessage, TypeInfo};
 
-use crate::{OperationName, OperationRef, BuilderId};
-
-use bevy_ecs::prelude::Component;
-
+use bevy_ecs::prelude::{Component, Entity, Event};
 use schemars::JsonSchema;
 use serde::{Serialize, Deserialize};
+use std::{any::Any, sync::Arc};
+use thiserror::Error as ThisError;
 
+/// A component attached to workflow operation entities in order to trace their
+/// activities.
 #[derive(Component)]
 pub struct Trace {
     info: Arc<OperationInfo>,
+    serialize_value: Option<fn(&dyn Any) -> Result<JsonMessage, GetValueError>>,
+}
 
+impl Trace {
+    /// Create trace information for an entity. By default this will not serialize
+    /// the messages passing through.
+    pub fn new(info: Arc<OperationInfo>) -> Self {
+        Trace { info, serialize_value: None }
+    }
+
+    /// Enable the trace for this operation to also send out the data of the
+    /// messages that are passing through.
+    pub fn enable_value_serialization<T: Any + Serialize>(&mut self) {
+        self.serialize_value = Some(get_serialize_value::<T>);
+    }
+
+    /// Get the information for this workflow operation.
+    pub fn info(&self) -> &Arc<OperationInfo> {
+        &self.info
+    }
+
+    /// Attempt to serialize the value. This will return a None if the trace is
+    /// not set up to serialize the values.
+    pub fn serialize_value(&self, value: &dyn Any) -> Option<Result<JsonMessage, GetValueError>> {
+        self.serialize_value.map(|f| f(value))
+    }
+}
+
+fn get_serialize_value<T: Any + Serialize>(value: &dyn Any) -> Result<JsonMessage, GetValueError> {
+    let Some(value_ref) = value.downcast_ref::<T>() else {
+        return Err(GetValueError::FailedDowncast {
+            expected: TypeInfo::of::<T>(),
+            received: std::any::TypeId::of::<T>(),
+        });
+    };
+
+    serde_json::to_value(value_ref)
+    .map_err(GetValueError::FailedSerialization)
+}
+
+#[derive(ThisError, Debug)]
+pub enum GetValueError {
+    #[error("The downcast was incompatible. Expected {expected:?}, received {received:?}")]
+    FailedDowncast {
+        expected: TypeInfo,
+        received: std::any::TypeId,
+    },
+    #[error("The serialization into json failed: {0}")]
+    FailedSerialization(serde_json::Error),
+}
+
+/// An event that gets transmitted whenever an operation receives an input or is
+/// activated some other way while tracing is on (meaning the operation entity
+/// has a [`Trace`] component).
+#[derive(Event)]
+pub struct OperationStarted {
+    /// The entity of the operation that was triggered.
+    pub entity: Entity,
+    /// Information about the operation that was triggered.
+    pub info: Arc<OperationInfo>,
+    /// The message that triggered the operation, if serialization is enabled
+    /// for it.
+    pub message: Option<JsonMessage>,
 }
 
 #[derive(Serialize, Deserialize, JsonSchema, Debug)]
@@ -50,7 +113,7 @@ impl OperationInfo {
         &self.construction
     }
 
-    pub fn op_name(&self) -> &Option<Arc<str>> {
+    pub fn op_label(&self) -> &Option<Arc<str>> {
         &self.op_label
     }
 }
