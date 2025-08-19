6377cd15c39407caa31e42ee22ce9b7cb27c9f3d
Michael X. Grey <greyxmike@gmail.com>
2025-08-19 22:11:43 +0800

    Fix e2e test
    Signed-off-by: Michael X. Grey <greyxmike@gmail.com>

--- examples/diagram/calculator/tests/e2e.rs
+++ examples/diagram/calculator/tests/e2e.rs
@@ -4,7 +4,7 @@ use assert_cmd::Command;
 fn multiply_by_3() {
     Command::cargo_bin("calculator")
         .unwrap()
-        .args(["run", "multiply_by_3.json", "4"])
+        .args(["run", "diagrams/multiply_by_3.json", "4"])
         .assert()
         .stdout("12.0\n");
 }
