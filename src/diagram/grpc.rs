4726e7f060aee61982254ae764ab724db8bdc040
Michael X. Grey <greyxmike@gmail.com>
2025-08-21 21:23:36 +0800

    Adding gRPC stream support
    Signed-off-by: Michael X. Grey <greyxmike@gmail.com>

--- src/diagram/grpc.rs
+++ src/diagram/grpc.rs
@@ -15,7 +15,13 @@
  *
 */
 
-use std::sync::Arc;
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::Arc,
+    task::{Context, Poll},
+    time::Duration,
+};
 use crate::AsyncMap;
 use super::*;
 
@@ -32,13 +38,33 @@ use http::uri::PathAndQuery;
 use serde::{Serialize, Deserialize};
 use schemars::JsonSchema;
 
-use futures::FutureExt;
+use futures::{
+    FutureExt,
+    channel::{
+        oneshot::{self, Sender as OneShotSender},
+        mpsc::{unbounded, UnboundedSender},
+    },
+    never::Never,
+};
+
+use futures_lite::future::race;
+
+use async_std::future::timeout;
 
 #[derive(Clone, Serialize, Deserialize, JsonSchema)]
 pub struct GrpcConfig {
     pub service: Arc<str>,
     pub method: Option<NameOrIndex>,
     pub uri: Box<[u8]>,
+    pub timeout: Option<Duration>,
+}
+
+#[derive(StreamPack)]
+struct GrpcStream {
+    out: JsonMessage,
+    stream_status: Status,
+    canceller: OneShotSender<Option<String>>,
+    input: ClientStreamInput,
 }
 
 #[derive(Clone, Serialize, Deserialize, JsonSchema)]
@@ -81,34 +107,125 @@ impl DiagramElementRegistry {
                     Client::new(channel)
                 }.shared();
 
+                let is_client_streaming = method.is_client_streaming();
+                let is_server_streaming = method.is_server_streaming();
+                let path = PathAndQuery::from_maybe_shared(path.clone()).unwrap();
+
                 builder.create_map(move |input: AsyncMap<JsonMessage, GrpcStream>| {
                     let client = client.clone();
                     let codec = codec.clone();
                     let path = path.clone();
                     async move {
                         let mut client = client.await;
+
+                        // Wait for client to be ready
                         client.ready().await.map_err(|e| format!("{e}"))?;
+                        if let Some(t) = config.timeout {
+                            timeout(t, client.ready())
+                                .await
+                                .map_err(|_| "timeout waiting for server to be ready".to_owned())?
+                                .map_err(|e| format!("server failed to be ready: {e}"))?;
+                        } else {
+                            client.ready().await.map_err(|e| format!("server failed to be ready: {e}"))?;
+                        }
+
+                        // Set up cancellation channel
+                        let (sender, receiver) = oneshot::channel();
+                        input.streams.canceller.send(sender);
+
+                        // Convert the request message into the appropriate type
                         let request = DynamicMessage::deserialize(codec.input.clone(), input.request)
                             .map_err(|e| format!("{e}"))?;
+                        let request = Request::new(request);
 
-                        let (_, r, _) = client
-                            .unary(
-                                Request::new(request),
-                                PathAndQuery::from_maybe_shared(path.clone()).map_err(|e| format!("{e}"))?,
-                                codec.clone(),
-                            )
-                            .await
-                            .map_err(|e| format!("{e}"))?
-                            .into_parts();
+                        if !is_client_streaming && !is_server_streaming {
+                            let session = client.unary(request, path, codec);
+                            let cancellable_session = race(session, receive_cancel(receiver));
 
-                        serde_json::to_value(r)
-                        .map_err(|e| format!("{e}"))
+                            let r = if let Some(t) = config.timeout {
+                                timeout(t, cancellable_session).await.map_err(|e| format!("{e}"))?
+                            } else {
+                                cancellable_session.await
+                            };
+
+                            let response = r.map_err(|e| format!("{e}"))?.into_inner();
+                            let value = serde_json::to_value(response)
+                                .map_err(|e| format!("failed to convert to json: {e}"))?;
+                            input.streams.out.send(value);
+
+                        } else if !is_client_streaming && is_server_streaming {
+                            let cancel = receiver.shared();
+
+                            let session = client.server_streaming(request, path, codec);
+                            let cancellable_session = race(session, receive_cancel(cancel.clone()));
+
+                            let r = if let Some(t) = config.timeout {
+                                timeout(t, cancellable_session).await.map_err(|e| format!("{e}"))?
+                            } else {
+                                cancellable_session.await
+                            };
+
+                            let mut streaming = r.map_err(|e| format!("{e}"))?.into_inner();
+                            loop {
+                                let r = if let Some(t) = config.timeout {
+                                    timeout(t, race(streaming.message(), receive_cancel(cancel.clone())))
+                                    .await
+                                    .map_err(|_| "timeout waiting for new stream message".to_owned())?
+                                } else {
+                                    race(streaming.message(), receive_cancel(cancel.clone())).await
+                                }
+                                .map_err(|e| format!("{e}"))?;
+
+                                let Some(response) = r else {
+                                    return Ok(());
+                                };
+
+                                let value = serde_json::to_value(response)
+                                    .map_err(|e| format!("failed to convert to json: {e}"))?;
+                                input.streams.out.send(value);
+                            }
+                        } else if is_client_streaming && !is_server_streaming {
+                            let (sender, receiver) = unbounded();
+
+                            let session = client.client_streaming(request, path, codec).await;
+
+                        }
+
+                        Ok::<_, String>(())
                     }
                 })
             }
         );
-
     }
+}
+
+async fn receive_cancel<T, E>(
+    receiver: impl Future<Output = Result<Option<String>, E>>,
+) -> Result<T, Status> {
+    match receiver.await {
+        Ok(msg) => {
+            // The user sent a signal to cancel this request
+            let msg = if let Some(msg) = msg {
+                format!("cancelled: {msg}")
+            } else {
+                format!("cancelled")
+            };
+
+            return Err::<T, _>(Status::new(Code::Cancelled, msg));
+        }
+        Err(_) => {
+            // The sender for the cancellation signal was dropped
+            // so we must never let this future finish.
+            NeverFinish.await;
+            unreachable!("this future will never finish");
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct ClientStreamInput {
+    sender: UnboundedSender<DynamicMessage>,
+    descriptor: MessageDescriptor,
 }
 
 #[derive(Clone)]
@@ -168,7 +285,14 @@ impl Decoder for DynamicMessageCodec {
     }
 }
 
-#[derive(StreamPack)]
-struct GrpcStream {
-    out: JsonMessage,
+/// This is used to block a future from ever returning. This should only be used
+/// in a race to force one of the contesting futures to lose. Make sure that at
+/// least one contesting future will finish or else this will lead to a deadlock.
+struct NeverFinish;
+
+impl Future for NeverFinish {
+    type Output = Never;
+    fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
+        Poll::Pending
+    }
 }
