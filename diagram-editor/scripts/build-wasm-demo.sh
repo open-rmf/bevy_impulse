6da83739b7f37f7ef03604722f7b6a786bbae3c4
Teo Koon Peng <teokoonpeng@gmail.com>
2025-09-10 14:11:26 +0800

    build and publish to gh-pages
    Signed-off-by: Teo Koon Peng <teokoonpeng@gmail.com>

--- diagram-editor/scripts/build-wasm-demo.sh
+++ diagram-editor/scripts/build-wasm-demo.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 set -euo pipefail
 
+: ${REPO_NAME:=bevy_impulse}
 wasm-pack build ../examples/diagram/calculator_wasm
-RSBUILD_OUTPUT_PREFIX=/bevy_impulse_diagram_editor_demo WASM_PKG_PATH=../examples/diagram/calculator_wasm/pkg WASM_PKG_NAME=calculator_wasm rsbuild build --environment wasm-backend
+RSBUILD_OUTPUT_PREFIX="/${REPO_NAME}" WASM_PKG_PATH=../examples/diagram/calculator_wasm/pkg WASM_PKG_NAME=calculator_wasm rsbuild build --environment wasm-backend
